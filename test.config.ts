245fac224d3fa7b181dc193c5b58be7e824e2b7b
Naveenkumar Rajeshkumar <naveenkumarr@unitedtechno.com>
2026-01-23 15:07:51 +0530

    Initial commit - Playwright TypeScript framework

---
+++ test.config.ts
@@ -0,0 +1,10 @@
+export class TestConfig{ 
+    
+    appUrl="https://www.henryschein.co.uk/" 
+       
+    //valid login credentials- create your own login account 
+    username="Naveenkumar" 
+    password="Welcome@123" 
+ 
+   
+}
