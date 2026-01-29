35eb01c807c9218abb57e8be688d338c7b69114d
GaneshBaskar15 <ganeshb@unitedtechno.com>
2026-01-29 16:07:47 +0530

    Added signup flow and search bar automation tests

--- pages/HomePage.ts
+++ pages/HomePage.ts
@@ -1,5 +1,4 @@
-import {Page,expect,Locator} from '@playwright/test'
-import path from 'node:path';
+import {Page,Locator} from '@playwright/test'
 
 export class HomePage { 
     //Locators
@@ -12,18 +11,53 @@ export class HomePage {
     public readonly user_account_tab_xpath: Locator;
     public readonly sign_in_error_icon_xpath: Locator;
     public readonly sign_in_error_msg_xpath: Locator;
+    public readonly accept_cookies_btn: Locator;
+    public readonly sign_up_xpath: Locator;
+    public readonly reg_username_input: Locator;
+    public readonly reg_email_input: Locator;
+    public readonly reg_password_input: Locator;
+    public readonly reg_confirm_password_input: Locator;
+    public readonly reg_next_step_btn: Locator;
+    public readonly continue_btn: Locator;
+    public readonly search_input: Locator;
+
+    
+
+
+
+
+    
+
+
 
     //constructor
     constructor(page:Page){
-       this.page=page
-       this.icon_xpath = page.locator("(//div[@class='logo'])[1]")
-       this.sign_in_xpath = page.locator("//button[contains(@id,'sign-in')]")
-        this.username_xpath = page.locator("//input[@data-placeholder='Username']")
-        this.password_xpath = page.locator("//input[@data-placeholder='password']")
-        this.sign_in_submit_btn_xpath=page.locator("//button[@type='submit']")
-        this.user_account_tab_xpath=page.locator("//li[@class='userdetails-mbl']")
-        this.sign_in_error_icon_xpath=page.locator("//div[@class='errorIcon']")
-        this.sign_in_error_msg_xpath=page.locator("//div[@class='errorIcon']/following::div[contains(@class,'errorMessage')]")
+       this.page=page;
+       this.icon_xpath = page.locator("(//div[@class='logo'])[1]");
+       this.sign_in_xpath = page.locator("//button[contains(@id,'sign-in')]");
+        this.username_xpath = page.locator("//input[@data-placeholder='Username']");
+        this.password_xpath = page.locator("//input[@data-placeholder='password']");
+        this.sign_in_submit_btn_xpath=page.locator("//button[@type='submit']");
+        this.user_account_tab_xpath=page.locator("//li[@class='userdetails-mbl']");
+        this.sign_in_error_icon_xpath=page.locator("//div[@class='errorIcon']");
+        this.sign_in_error_msg_xpath=page.locator("//div[@class='errorIcon']/following::div[contains(@class,'errorMessage')]");
+        this.sign_up_xpath = page.locator("//span[@class='signUp ng-star-inserted']");
+        this.reg_username_input = page.locator("[data-test-id='username-input']");
+        this.accept_cookies_btn = page.locator("//button[contains(text(),'Accept') or contains(text(),'Allow')]");
+        this.reg_email_input = page.locator("[data-test-id='email-input']");
+        this.reg_password_input = page.locator("[data-test-id='new-pass-Input']");
+        this.reg_confirm_password_input = page.locator("[data-test-id='confirm-pass-input']");
+        this.reg_next_step_btn = page.locator("[data-test-id='next-step-button']");
+        this.continue_btn = page.locator("//button[normalize-space()='Continue']");
+        this.search_input = page.locator("//input[@data-test-id='RecipientUsername']");
+
+
+        
+
+        
+
+
+
     //Action methods
     }
     async isHomePageExist(){
