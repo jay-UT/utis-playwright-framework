3524a66a589b16ca7140f0953306c2ac78ec7384
Gopikasri123 <gopikasrip@unitedtechno.com>
2026-02-04 13:52:24 +0530

    Added Checkout and Cart Scenarios

--- pages/HomePage.ts
+++ pages/HomePage.ts
@@ -12,10 +12,10 @@ export class HomePage {
     public readonly user_account_tab_xpath: Locator;
     public readonly sign_in_error_icon_xpath: Locator;
     public readonly sign_in_error_msg_xpath: Locator;
-    public readonly address_tooltip_xpath:Locator;
-    public readonly sliderBanner_xpath:Locator;
-    public readonly sliderBannerActive_xpath:Locator;
-    public readonly slideNavButton:Locator;
+    public readonly address_tooltip_xpath: Locator;
+    public readonly sliderBanner_xpath: Locator;
+    public readonly sliderBannerActive_xpath: Locator;
+    public readonly slideNavButton: Locator;
     public readonly main_menu: Locator;
     public readonly main_menu_Corporate: Locator;
     public readonly main_menu_About_HenrySchein: Locator;
@@ -45,6 +45,15 @@ export class HomePage {
     public readonly reg_next_step_btn: Locator;
     public readonly continue_btn: Locator;
     public readonly search_input: Locator;
+    public readonly product_1: Locator;
+    public readonly view_basket_qty: Locator;
+    public readonly Add_to_basket: Locator;
+    public readonly cart_items: Locator;
+    public readonly Proceed_Shipping_Billing: Locator;
+    public readonly Cart_Page: Locator;
+    public readonly Review_Order: Locator;
+    public readonly Review_Order_Page: Locator;
+    public readonly view_basket: Locator;
 
     //constructor
     constructor(page: Page) {
@@ -53,10 +62,10 @@ export class HomePage {
         this.sign_in_xpath = page.locator("//button[contains(@id,'sign-in')]");
         this.username_xpath = page.locator("//input[@data-placeholder='Username']");
         this.password_xpath = page.locator("//input[@data-placeholder='password']");
-        this.sign_in_submit_btn_xpath=page.locator("//button[@type='submit']");
-        this.user_account_tab_xpath=page.locator("//li[@class='userdetails-mbl']");
-        this.sign_in_error_icon_xpath=page.locator("//div[@class='errorIcon']");
-        this.sign_in_error_msg_xpath=page.locator("//div[@class='errorIcon']/following::div[contains(@class,'errorMessage')]");
+        this.sign_in_submit_btn_xpath = page.locator("//button[@type='submit']");
+        this.user_account_tab_xpath = page.locator("//li[@class='userdetails-mbl']");
+        this.sign_in_error_icon_xpath = page.locator("//div[@class='errorIcon']");
+        this.sign_in_error_msg_xpath = page.locator("//div[@class='errorIcon']/following::div[contains(@class,'errorMessage')]");
         this.sign_up_xpath = page.locator("//span[@class='signUp ng-star-inserted']");
         this.reg_username_input = page.locator("[data-test-id='username-input']");
         this.accept_cookies_btn = page.locator("//button[contains(text(),'Accept') or contains(text(),'Allow')]");
@@ -66,11 +75,11 @@ export class HomePage {
         this.reg_next_step_btn = page.locator("[data-test-id='next-step-button']");
         this.continue_btn = page.locator("//button[normalize-space()='Continue']");
         this.search_input = page.locator("//input[@data-test-id='RecipientUsername']");
-        this.address_tooltip_xpath=page.locator("(//div[@class='address-info cursor-pointer'])[1]")
+        this.address_tooltip_xpath = page.locator("(//div[@class='address-info cursor-pointer'])[1]")
         this.sliderBanner_xpath = page.locator("//div[@class='carousel slide hero-carousel-container']")
-        this.sliderBannerActive_xpath=page.locator("//div[@class='carousel-item active']")
-        this.slideNavButton=page.locator("(//span[@class='carousel-control-next-icon'])[1]")
-    //Action methods
+        this.sliderBannerActive_xpath = page.locator("//div[@class='carousel-item active']")
+        this.slideNavButton = page.locator("(//span[@class='carousel-control-next-icon'])[1]")
+        //Action methods
         this.sign_in_submit_btn_xpath = page.locator("//button[@type='submit']")
         this.user_account_tab_xpath = page.locator("//li[@class='userdetails-mbl']")
         this.sign_in_error_icon_xpath = page.locator("//div[@class='errorIcon'][1]")
@@ -78,7 +87,6 @@ export class HomePage {
         this.main_menu = page.locator("//span[@data-test-id='mega_menu_component_span_41']")
         this.main_menu_Corporate = page.locator("//a[@data-test-id='menu_list_component_a_4' and contains(@title,'Corporate')]")
         this.main_menu_About_HenrySchein = page.locator("//a[@data-test-id='menu_list_component_a_4' and contains(@title,'About Henry Schein')]")
-        //main_menu_Blog = //a[@data-test-id='menu_list_component_a_4' and contains(@title,'Blog')]
         this.main_menu_Top_Supplies = page.locator("//button[@data-test-id='mega_menu_component_button_37']//span[contains(normalize-space(),'Top Supplies')]")
         this.main_menu_Top_Supplies_detail = page.locator("//div[contains(@class,'equipment-details')]//h3[contains(normalize-space(),'Top Supplies')]")
         this.main_menu_Top_Equipment = page.locator("//button[@data-test-id='mega_menu_component_button_37']//span[contains(normalize-space(),'Top Equipment')]")
@@ -92,11 +100,19 @@ export class HomePage {
         this.main_menu_My_Dashboard = page.locator("//button[@data-test-id='mega_menu_component_button_37']//span[contains(normalize-space(),'My Dashboard')]")
         this.main_menu_Order_From_History = page.locator("//span[@data-test-id='megaMenu.NavigationTitle2' and contains(normalize-space(),'Order From History')]")
         this.main_menu_My_Dashboard_details = page.locator("//div[@class='tab-pane collapse show']")
-        this.footer_Legal_Terms=page.locator("//a[text()=' Legal Terms ']")
-        this.footer_Privacy_Notice=page.locator("//a[text()=' Privacy Notice ']")
+        this.footer_Legal_Terms = page.locator("//a[text()=' Legal Terms ']")
+        this.footer_Privacy_Notice = page.locator("//a[text()=' Privacy Notice ']")
         this.footer_Delivery_and_Returns = page.locator("//a[text()=' Delivery and Returns ']")
         this.footer_LinkedIn = page.locator("//a[@text='Follow HenrySchein on linkedIn link ']")
-        
+        this.product_1 = page.locator("//div[@data-test-id='productdetails-standalone' and contains(.,'DIS258')]")
+        this.Add_to_basket = page.locator("(//button[@data-test-id='pdp_button_addcart'])[1]")
+        this.view_basket_qty = page.locator("//span[@data-test-id='cart_image_qty']")
+        this.cart_items = page.locator("//h4[@data-test-id='cart-productname']")
+        this.Proceed_Shipping_Billing = page.locator("//button[@data-test-id='cart_button_shippingbilling']")
+        this.Cart_Page = page.locator("//span[@data-test-id='shippingBilling.ShippingBillingText1']")
+        this.Review_Order = page.locator("//button[@data-test-id='shipping_button_revieworder']")
+        this.Review_Order_Page = page.locator("//h1[@data-test-id='reviewOrder.ReviewOrderTitleText3']")
+        this.view_basket = page.locator("//img[@data-test-id='cart_image_icon']")
 
     }
     async isHomePageExist() {
