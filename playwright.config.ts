909a7ad393a8b1d2e533c35b2a0b285e52f025d4
Shaarif <mohammeds@unitedtechno.com>
2026-02-09 21:49:16 +0530

    Updated the regression.yml file

--- playwright.config.ts
+++ playwright.config.ts
@@ -31,7 +31,7 @@ export default defineConfig({
   forbidOnly: isCI,
 
   /* Retry on CI only */
-  retries: isCI ? 2 : 0,
+  retries: isCI ? 1 : 0,
 
   /* Opt out of parallel tests on CI */
   workers: isCI ? 1 : 1,
